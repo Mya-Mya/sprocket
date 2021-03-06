cd9bb9d81cab484a5e422748140f0160e0bea0f3
Mya-Mya <interleukin413@gmail.com>
2021-03-06 11:57:15 +0900

    ローカットフィルターを通すか通さないかによって関数を切り替える

--- realtime/util/audioio.py
+++ realtime/util/audioio.py
@@ -7,10 +7,11 @@ from scipy.signal import firwin, lfilter
 _audio = pyaudio.PyAudio()
 
 
-def get_available_input_devices()->dict:
+def get_available_input_devices() -> dict:
     return {i: _audio.get_device_info_by_index(i)['name'] for i in range(_audio.get_device_count())}
 
-def get_available_output_devices()->dict:
+
+def get_available_output_devices() -> dict:
     return {i: _audio.get_device_info_by_index(i)['name'] for i in range(_audio.get_device_count())}
 
 
@@ -19,7 +20,7 @@ class Microphone:
                  frames_per_buffer: int,
                  frame_rate: int,
                  input_device_index: int,
-                 cutoff:int = 70
+                 cutoff: int = 70
                  ):
         self._stream = _audio.open(
             format=pyaudio.paInt16,
@@ -29,22 +30,35 @@ class Microphone:
             input=True
         )
         self._frame_per_buffer = frames_per_buffer
-        self._lowcutfilter = firwin(255, cutoff / (frame_rate // 2), pass_zero=False)
+        if cutoff == 0:
+            self._read_frame_impl = self._read_frame_wo_lowpassfilter
+        else:
+            self._read_frame_impl = self._read_frame_with_lowpassfilter
+            self._lowcutfilter = firwin(255, cutoff / (frame_rate // 2), pass_zero=False)
         atexit.register(self.close_stream)
 
-    def read_frame(self)->numpy.ndarray:
+    def read_frame(self) -> numpy.ndarray:
         '''
         Returns
         -------
         1フレーム内の音声情報
         '''
+        return self._read_frame_impl()
+
+    def _read_frame_with_lowpassfilter(self):
         wav_bytes = self._stream.read(self._frame_per_buffer)
-        wav_array = numpy.frombuffer(wav_bytes,dtype=numpy.core.int16)
+        wav_array = numpy.frombuffer(wav_bytes, dtype=numpy.core.int16)
         wav_array = lfilter(self._lowcutfilter, 1, wav_array)
+        return wav_array.astype(numpy.float)
+
+    def _read_frame_wo_lowpassfilter(self):
+        wav_bytes = self._stream.read(self._frame_per_buffer)
+        wav_array = numpy.frombuffer(wav_bytes, dtype=numpy.core.int16)
         return wav_array
 
     def close_stream(self):
         self._stream.close()
+
 
 class Speaker:
     def __init__(self,
@@ -62,7 +76,7 @@ class Speaker:
         )
         atexit.register(self.close_stream)
 
-    def play_frame(self,wav_data:numpy.ndarray):
+    def play_frame(self, wav_data: numpy.ndarray):
         self._stream.write(wav_data.tobytes())
 
     def close_stream(self):
